31f27e1149ae99003b7eb82c880505f1d6419a45
Robert Allred <rallred84@gmail.com>
2023-08-19 21:50:51 -0500

    Experimented adding Quill rich text editor

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,20 @@
 import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import parse from "html-react-parser";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike"],
+    [{ color: [] }, { background: [] }],
+    ["blockquote"],
+    [{ list: "ordered" }, { list: "bullet" }],
+    [{ indent: "-1" }, { indent: "+1" }, { align: [] }],
+    ["link", "image", "video"],
+    ["clean"],
+  ],
+};
 
 const Home = () => {
   const [posts, setPosts] = useState([`Testing`, "Again"]);
@@ -16,18 +32,24 @@ const Home = () => {
       <h3>Testing Posts</h3>
       {posts.map((post) => {
         return (
-          <p key={Math.random()} className="post-text">
-            {post}
-          </p>
+          <div key={Math.random()} className="post-text">
+            {parse(post)}
+          </div>
         );
       })}
       <form action="" onSubmit={(e) => handleAddPost(e)}>
-        <textarea
+        {/* <textarea
           value={post}
           cols="30"
           rows="10"
           onChange={(e) => setPost(e.target.value)}
-        ></textarea>
+        ></textarea> */}
+        <ReactQuill
+          theme="snow"
+          modules={modules}
+          placeholder="Content goes here..."
+          onChange={setPost}
+        />
         <button>Add Post</button>
       </form>
     </>
